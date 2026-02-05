6cf8d79e46bf80eac9336e2329b5add4a23c3fb9
sujaljd <zaynog2911@gmail.com>
2026-02-05 20:11:05 +0530

    input_output

---
+++ input_output.py
@@ -0,0 +1,13 @@
+import time
+
+age=int(input("Please enter your age: "))
+if age<18:
+    print("You are still a minor.")
+else:
+    print("You are an adult. you can appply for driving licence, please fill the required information carefully.")
+    name=input("Please fill the required information:\nName: ")
+    address=input("Adress: ")
+    phone=input("Phone number: ")
+    time.sleep(4)
+    print("Verified...your driving licence will br post to your adress")
+    
